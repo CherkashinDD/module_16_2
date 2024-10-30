76d8297a43568e4c83dc16849ede0bad253d653f
CherkashinDD <danl-0284@mail.ru>
2024-10-30 10:05:46 +1000

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -21,7 +21,7 @@ async def get_user_id(user_id: int = Path(ge=1, le=100, description='Enter User 
 
 @app.get("/user/{username}/{age}")
 async def get_user_info(
-        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example="Alex")],
+        username: Annotated[str, Path(min_length=4, max_length=20, description='Enter username', example="Alex")],
         age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="40")]
     ):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
